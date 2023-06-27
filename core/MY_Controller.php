3b75132856e98e655f7292838bf958ab95e5c222
Sunu Aziz Rahayu <me@kangsunu.com>
2023-06-27 10:59:00 +0700

    fix form validation on hmvc

--- core/MY_Controller.php
+++ core/MY_Controller.php
@@ -1,7 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MY_Controller extends MX_Controller {}
+class MY_Controller extends MX_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		if (!empty($this->form_validation)) {
+			$this->form_validation->CI =& $this;
+		}
+	}
+
+}
 
 /* End of file MY_Controller.php */
 /* Location: ./application/core/MY_Controller.php */
---
+++ libraries/MY_Form_validation.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Form_validation extends CI_Form_validation {
+
+	public $CI;
+
+}
+
+/* End of file MY_Form_validation.php */
+/* Location: .//home/dev/Documents/PROJECT/CodeIgniter-HMVC-Modular-Extension/libraries/MY_Form_validation.php */
